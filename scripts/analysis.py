83aba073c9a8258157642822932b76eaef009de7
gyutaepark <gtpark96@gmail.com>
2017-10-27 18:59:50 -0500

    Added initial scrip

---
+++ scripts/analysis.py
@@ -0,0 +1,66 @@
+import sqlite3 as sql
+
+
+def get_cursor(file_name):
+    """ Connects and returns a cursor to an sqlite output file
+
+    Parameters
+    ----------
+    file_name: str
+        name of the sqlite file
+
+    Returns
+    -------
+    sqlite cursor3
+    """
+    con = sql.connect(file_name)
+    con.row_factory = sql.Row
+    return con.cursor()
+
+
+def get_agent_ids(cur, archetype):
+    """ Gets all agentIds from Agententry table for wanted archetype
+
+        agententry table has the following format:
+            SimId / AgentId / Kind / Spec /
+            Prototype / ParentID / Lifetime / EnterTime
+
+    Parameters
+    ----------
+    cur: cursor
+        sqlite cursor3
+    archetype: str
+        agent's archetype specification
+
+    Returns
+    -------
+    id_list: list
+        list of all agentId strings
+    """
+    agents = cur.execute("SELECT agentid FROM agententry WHERE spec "
+                         "LIKE '%" + archetype + "%' COLLATE NOCASE"
+                         ).fetchall()
+
+    return list(str(agent['agentid']) for agent in agents)
+
+
+def get_prototype_ids(cur, prototype):
+    """ Returns agentid of a prototype
+
+    Parameters
+    ----------
+    cur: sqlite cursor
+        sqlite cursor
+    prototype: str
+        name of prototype
+
+    Returns
+    -------
+    agent_id: list
+        list of prototype agent_ids as strings
+    """
+    ids = cur.execute('SELECT agentid FROM agententry '
+                      'WHERE prototype = "' +
+                      str(prototype) + '" COLLATE NOCASE').fetchall()
+
+    return list(str(agent['agentid']) for agent in ids)
