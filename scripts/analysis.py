5ac856f046e92b83db7c0670d0bc2875c7e6107b
gyutaepark <gtpark96@gmail.com>
2018-01-28 17:44:03 -0600

    Update

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,4 +1,5 @@
 from mpl_toolkits.basemap import Basemap
+from shapely.geometry import Point
 import geopandas as gpd
 import sqlite3 as sql
 import matplotlib.pyplot as plt
@@ -22,67 +23,57 @@ def get_cursor(file_name):
     return con.cursor()
 
 
-def get_agent_ids(cur, archetype):
-    """ Gets all agentIds from Agententry table for wanted archetype
-
-        agententry table has the following format:
-            SimId / AgentId / Kind / Spec /
-            Prototype / ParentID / Lifetime / EnterTime
-
-    Parameters
-    ----------
-    cur: cursor
-        sqlite cursor3
-    archetype: str
-        agent's archetype specification
-
-    Returns
-    -------
-    id_list: list
-        list of all agentId strings
-    """
-    agents = cur.execute("SELECT agentid FROM agententry WHERE spec "
-                         "LIKE '%" + archetype + "%' COLLATE NOCASE"
-                         ).fetchall()
-
-    return list(str(agent['agentid']) for agent in agents)
-
-
-def get_prototype_ids(cur, prototype):
-    """ Returns agentid of a prototype
+def available_archetypes(cur):
+    """ Returns list of all CYCAMORE archetypes
+    excluding ManagerInst, DeployInst, and GrowthRegion.
 
     Parameters
     ----------
     cur: sqlite cursor
         sqlite cursor
-    prototype: str
-        name of prototype
 
     Returns
-    -------
-    agent_id: list
-        list of prototype agent_ids as strings
+    -------list
+        list of archetypes available for plotting
     """
-    ids = cur.execute('SELECT agentid FROM agententry '
-                      'WHERE prototype = "' +
-                      str(prototype) + '" COLLATE NOCASE').fetchall()
-
-    return list(str(agent['agentid']) for agent in ids)
+    blacklist = {'ManagerInst',
+                 'DeployInst',
+                 'GrowthRegion'}
+    query = cur.execute("SELECT spec FROM agentposition WHERE spec"
+                        " LIKE '%cycamore%' COLLATE NOCASE")
+    archetypes = {agent['spec'][10:] for agent in query} - blacklist
+    return list(archetypes)
 
 
 def get_archetype_position(cur, archetype):
-    pos = cur.execute("SELECT agentid, spec, prototype, latitude, longitude "
-                      "FROM agentposition WHERE spec LIKE '%" + archetype +
-                      "%' COLLATE NOCASE")
+    """ Makes an sqlite query to AgentPosition table to obtain
+    agentid, spec, prototype, latitude, and longitude 
 
-    return {str(agent['agentid']): (agent['spec'][10:], agent['prototype'],
-                                    (agent['latitude'], agent['longitude']))
-            for agent in pos}
+    Parameters
+    ----------
+    cur: sqlite cursor
+        sqlite cursor
+
+    Returns
+    -------
+    positions: dict
+        dictionary with key=[agentid], 
+        and value=[list of prototype, spec, latitude, longitude]
+    """
+    query = cur.execute("SELECT agentid, spec, prototype, latitude, longitude "
+                        "FROM agentposition WHERE spec LIKE '%" + archetype +
+                        "%' COLLATE NOCASE")
+    positions = {str(agent['agentid']): [agent['prototype'],
+                                         agent['spec'][10:],
+                                         agent['latitude'],
+                                         agent['longitude']]
+                 for agent in query}
+    return positions
 
 
-def available_archetypes(cur):
-	""" Returns agentid of all CYCAMORE archetypes
-	excluding ManagerInst, DeployInst, and GrowthRegion.
+def archetype_dataframe(cur, archetype):
+    """ Returns agentid of all CYCAMORE archetypes
+    excluding ManagerInst, DeployInst, and GrowthRegion.
 
     Parameters
     ----------
@@ -94,18 +85,22 @@ def available_archetypes(cur):
     archetypes: set
         set of archetypes available for plotting
     """
-    blacklist = {'ManagerInst',
-                 'DeployInst',
-                 'GrowthRegion'}
-    archetypes = get_archetype_position(cur, 'cycamore')
-    archetypes = {v[0] for k, v in archetypes.items()}
-    archetypes -= blacklist
-    return archetypes
+    query = get_archetype_position(cur, archetype)
+    positions_df = {}
+    for k, v in query.items():
+        positions_df['agentid'] = 
+
+    positions_df = {'agentid': [agent['agentid'] for agent in query],
+                    'prototype': [agent['prototype'] for agent in query],
+                    'spec': [agent['spec'] for agent in query],
+                    'geometry': [Point(agent['latitude'],
+                                       agent['longitude']) for agent in query]}
+    return gpd.GeoDataFrame(positions_df)
 
 
 def get_bounds(cur):
-	""" Returns the upper and lower limits of latitude
-	and longitude for use with Basemap.
+    """ Returns the upper and lower limits of latitude
+    and longitude for use with Basemap.
 
     Parameters
     ----------
@@ -125,18 +120,17 @@ def get_bounds(cur):
     for arch in archs:
         arch_dict = get_archetype_position(cur, arch)
         prototypes = {**prototypes, **arch_dict}
-    coordinates = [tup[2] for tup in list(prototypes.values())]
-    llcrnrlat = min(coordinates[0])
-    llcrnrlon = min(coordinates[1])
-    urcrnrlat = max(coordinates[0])
-    urcrnrlon = max(coordinates[1])
-    
+    latitudes = [info[2] for info in list(prototypes.values())]
+    longitudes = [info[3] for info in list(prototypes.values())]
+    llcrnrlat = min(latitudes)
+    llcrnrlon = min(longitudes)
+    urcrnrlat = max(latitudes)
+    urcrnrlon = max(longitudes)
     bounds = [llcrnrlat, llcrnrlon, urcrnrlat, urcrnrlon]
     return bounds
 
 
 def plot_agents(cur):
-    shapes = ['o', 'v', 's', 'p', '8', 'H']
     fig = plt.figure(figsize=(20, 15))
     bounds = get_bounds(cur)
     sim_map = Basemap(projection='cyl',
@@ -146,11 +140,8 @@ def plot_agents(cur):
                       urcrnrlon=bounds[3])
     sim_map.drawcoastlines()
     sim_map.drawcountries()
-
-    for i, archetype in enumerate(available_archetypes(cur)):
-        archetypes = get_archetype_position(cur, archetype)
-        for k, v in archetypes.items():
-            plt.scatter(v[2][1], v[2][0], marker=shapes[i], label=v[0])
-            plt.annotate(v[1], xy=v[2])
-
+    archs = {}
+    for archetype in available_archetypes(cur):
+        archs[archetype] = archetype_dataframe(cur, archetype)
+        archs[archetype].plot()
     plt.show()
