6fbe645fbe8d1c52c559c537236026b75e80e496
gyutaepark <gtpark96@gmail.com>
2018-02-28 16:23:11 -0600

    fixed unwanted overlaps that hid map objects

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -361,10 +361,10 @@ def plot_basemap(cur):
                       urcrnrlat=bounds[2],
                       urcrnrlon=bounds[3])
     sim_map.drawcoastlines(zorder=-15)
-    sim_map.drawcountries(zorder=-10)
-    sim_map.drawstates(zorder=-10)
-    sim_map.fillcontinents(color='white', lake_color='aqua', zorder=-10)
-    sim_map.drawmapboundary(fill_color='lightblue', zorder=0)
+    sim_map.drawmapboundary(fill_color='lightblue', zorder=-10)
+    sim_map.fillcontinents(color='white', lake_color='aqua', zorder=-5)
+    sim_map.drawcountries(zorder=0)
+    sim_map.drawstates(zorder=0)
     return sim_map
 
 
